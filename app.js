9ed5aca8a5ab0836a1aec8f72f55c62d6c6bf5b6
secondubly <secondubly@gmail.com>
2021-05-23 17:45:55 -0400

    we finished the basics! (cleanup later)

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 5353
-const path = require('path')
+const port = process.env.PORT || 5353;
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { response } = require('express');
+const io = new Server(server);
 
-// static files are located in /public directory, this tells express to serve them
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (request, response) => {
-    // send the default index.html file when getting a request to the root directory
-    response.sendFile('index.html', {root: __dirname })
-});
+app.get('/clippy', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+})
 
-app.listen(port, () => {
+// http://localhost:5353/clippy/%3Cchannelname%3E?streamer=%3Cusername%3E
+// http://localhost:5353/clippy/secondubly?streamer=datto
+// https://localhost:5353/clippy
+app.get('/clippy/:host', (req, res) => {
+    if(isBlank(req.params.host)) {
+        res.sendFile('index.html', { root: __dirname });
+    } else {
+    io.emit('shoutout', { host: req.params.host, streamer: req.query.streamer });
+    res.sendFile('index.html', { root: __dirname });
+    }
+})
+
+server.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
 })
+
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
--- public/js/clippy.js
+++ public/js/clippy.js
@@ -1,10 +1,12 @@
 let topClips = [];
-let distribution = {};
+let distribution = {}; // for testing purposes
 
-async function getClips(limit = 100, username = 'secondubly', period = 'all', weighted = true) {
+async function getClips(username, limit = 100, period = 'all', weighted = true, trending = false) {
     // TODO: sanitize input
     // Note: await/async unsupported in some older browsers (IE/Opera?)
-    const data = await fetch(`https://api.twitch.tv/kraken/clips/top?channel=${username}&limit=${limit}&period=${period}`, {
+    let url = `https://api.twitch.tv/kraken/clips/top?channel=${username}&limit=${limit}&period=${period}&trending=${trending}`
+    console.log(url);
+    const data = await fetch(`https://api.twitch.tv/kraken/clips/top?channel=${username}&limit=${limit}&period=${period}&trending=${trending}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -12,62 +14,80 @@ async function getClips(limit = 100, username = 'secondubly', period = 'all', we
             'Accept': 'application/vnd.twitchtv.v5+json'
         }
     }).then(res => res.json());
-
+    console.log(data.clips);
     if (data.clips.length < 1) { return; } // if we don't have any clips, return early
 
-    topClips = data.clips;
+    // if the clip is longer than the duration limit, we don't want it
+    topClips = data.clips
+    // topClips = data.clips.filter(clip => clip.duration < duration_limit);
 }
 
-/**
- * Returns an index of 
- * @param {Array.<Object>} clips 
- * @param {number} percentage A decimal number that signifies the weighted percentage - the default is 0.10 (10%)
- * @returns {number} index of the array item that was randomly selected
- */
-function weightedRandom (clips, percentage = 0.10) {
-    // first we need to generate the weight ranges for each item
-    let weights = [0]; // this array will hold a range starting from 0 that represents percentages [0, 1)
-    let total_weight_sum = 0
-    // generate weights for each item using the percentage
-    for(let i = 0; i < clips.length; i++) {
+function getRandomClip(clips, percentage = 0.50) {
+    // first generate the weight for each clip
+    // weights = [0, .7, .2, .0523...]
+    let weights = [0]; // holds the weights for each clip (skipping position 0)
+    let total_weight = 0;
+    
+    // generate weights for each clip, using the percentage
+    // a, b, c
+    // 0, 1, 2 = i
+    for (let i = 0; i < clips.length; i++) {
         let item_weight = 1 / (1 + i) * percentage;
         weights.push(item_weight);
-        total_weight_sum += item_weight; // add up the total weight for ALL items, we'll use this later
+        total_weight += item_weight; // add up the total weight for ALL items (this is important for later)
     }
 
-    // pick a random value threshold, multiply it by the sum of all the weights (this gives it an upper limit)
-    const threshold = Math.random() * total_weight_sum;
-    // console.log(`Threshold Value: ${threshold}`)
-
-    // now loop over all the weights - if the value is within the threshold then we've picked a clip
-    total_weight_sum = 0;
-    for (let i = 0; i < weights.length - 1; ++i) {
+    // pick a random number between 0 and 1 (but NOT 1), and multiply it by the total weight
+    // to give an upper limit
+    const upper_limit = Math.random() * total_weight;
+    total_weight = 0;
+    for(let i = 0; i < weights.length; ++i) {
         // add the weight to our running total (which starts at 0)
-        total_weight_sum += weights[i];
-        // if this total weight is greater than the threshold value
-        // aka if the threshold is 60 and the weight is 35
-        // then we are in the range we need, so just return where we're at
-        if(total_weight_sum >= threshold) {
-            // return clips[i - 1];
-            return i - 1;
+        total_weight += weights[i];
+
+        // if this total weight is greater than the upper limit
+        // then we are in the proper range, so return the clip
+        if (total_weight >= upper_limit) {
+            return clips[i - 1];
+            // return i - 1; // for testing purposes
         }
     }
-
-    return clips.length - 1; // in case the previous loop fails, return the very last item!
+    // in case the loop fails (why would it?)
+    return clips[clips.length - 1];
+    // return clips.length - 1; // for testing purposes
 }
 
-getClips().then(_ => {
-    if(topClips.length > 0) {
-        let index = 0;
-        // setup distribution map
-        for (const _ of topClips) {
-            distribution[index] = 0;
-            index++;
-        }
-        for (let i = 0; i < 10000; i++) {
-            const clip_index = weightedRandom(topClips);
-            distribution[clip_index]++;
-        }
-        console.log(distribution);
+function setClip(visible, clip) {
+    if (visible) {
+        console.log(clip);
+        let frame = document.querySelector("#stream-clip")
+        let container = document.querySelector('#clip-container');
+
+        // Example Clip URL:
+        // https://clips.twitch.tv/embed?clip=ShakingPoliteQuailGingerPower&tt_medium=clips_api&tt_content=embed
+        const clip_url = `http://clips.twitch.tv/embed?clip=${clip.slug}&tt_medium-clips_api&tt_content=embed&parent=${window.location.hostname}&autoplay=true`
+
+        // display container and iframe
+        frame.setAttribute('src', clip_url);
+        frame.style.display = 'block';
+
+        container.style.display = 'block';
+        setTimeout(() => {
+            // auto-hide clip after it finishes
+            setClip(false, null);
+        }, clip.duration * 1000);
+    } else {
+        // Don't show clip box
+        let container = document.querySelector('#clip-container');
+        let frame = document.querySelector("#stream-clip");
+
+        // hide container
+        container.style.display = 'none';
+
+        // hide iframe
+        frame.setAttribute('src', '');
+        frame.style.display = 'none';
     }
-}).catch();
+}
+
+
