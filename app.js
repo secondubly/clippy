e24038314a430e30172e0d0dea1291b55f765c8f
secondubly <secondubly@gmail.com>
2021-05-24 10:55:09 -0400

    cleaned up routing

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const { response } = require('express');
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/clippy', (req, res) => {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendStatus(404);
 })
 
-// http://localhost:5353/clippy/%3Cchannelname%3E?streamer=%3Cusername%3E
-// http://localhost:5353/clippy/secondubly?streamer=datto
-// https://localhost:5353/clippy
 app.get('/clippy/:host', (req, res) => {
-    if(isBlank(req.params.host)) {
+    if(isBlank(req.query.streamer) && req.params.host) {
         res.sendFile('index.html', { root: __dirname });
     } else {
-    io.emit('shoutout', { host: req.params.host, streamer: req.query.streamer });
-    res.sendFile('index.html', { root: __dirname });
+        io.emit('shoutout', { host: req.params.host, streamer: req.query.streamer });
+        res.sendStatus(200);        
     }
 })
 
